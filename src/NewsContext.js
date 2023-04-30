09d803e88e31b73b0acd416c643b82c24d2390c6
DESKTOP-9Q41CN2\Reigh <alyanrizky@gmail.com>
2023-04-30 12:35:33 +0700

    first commit

--- src/NewsContext.js
+++ src/NewsContext.js
@@ -5,19 +5,24 @@ export const NewsContext = createContext();
 
 export const NewsContextProvider = (props) => {
   const [data, setData] = useState();
+  const [searchQuery, setSearchQuery] = useState("Apple"); // set default search query
   const apiKey = "d3a68d3a93a54948a016a1553bc4d20c";
 
   useEffect(() => {
     axios
       .get(
-        `https://newsapi.org/v2/everything?q=Apple&from=2023-02-28&sortBy=popularity&apiKey=${apiKey}`
+        `https://newsapi.org/v2/everything?q=${searchQuery}&from=2023-02-28&sortBy=popularity&apiKey=${apiKey}`
       )
       .then((response) => setData(response.data))
       .catch((error) => console.log(error));
-  }, []);
+  }, [searchQuery]); // watch for changes to searchQuery and update data accordingly
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
 
   return (
-    <NewsContext.Provider value={{ data }}>
+    <NewsContext.Provider value={{ data, handleSearch }}>
       {props.children}
     </NewsContext.Provider>
   );
